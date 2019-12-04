64ec7c0af2d4e80210208912c465c85ec2b6d8cb
LUCAS ALVES DE LACERDA <631610108@fspoa.br>
2019-12-04 20:47:50 -0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+var port = process.env.PORT || 3000;
+
+const authController = require('./controller/authentication');
+
+app.listen(port, () => {
+    console.log('Listen to port 3000')
+});
